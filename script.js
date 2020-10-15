bd180aa852179fdc18dd3fc2349a31485b860276
Amanda Rodriguez <hair.web420@gmail.com>
2020-10-14 22:42:49 -0600

    updated working function

--- script.js
+++ script.js
@@ -10,21 +10,14 @@ function writePassword() {
 }
 function generatePassword() {
   var passwordLength = parseInt(
-    prompt("How many characters do you want your password?")
-  );
-  // var passwordLength2 = prompt(
-  //   "How many characters do you want your password?"
-  // );
-  // console.log(typeof passwordLength);
-  // console.log(typeof passwordLength2);
+    prompt("How many characters do you want your password?"));
   var upperCase = confirm("Do you want to use uppercase letters?");
   var lowerCase = confirm("Do you want to use lowercase letters?");
   var numeric = confirm("Do you want to use any numerics?");
   var specialCharacters = confirm("Do you want to use special characters?"); 
 
   if (passwordLength >= 8 && passwordLength <= 128) {
-    if (upperCase || lowerCase || numeric || specialCharacters) {
-        // depeding on whivh options they choose will be the ones i generate for loop will generate.
+    if (upperCase || lowerCase || numeric || specialCharacters) {   
     } else {
       alert("Please choose at least one option");
       return writePassword();
@@ -32,8 +25,14 @@ function generatePassword() {
   } else {
     alert("Please choose a password between 8 and 128!");
     return writePassword();
-  };
+
+  }
+  for (var i = 0; i < passwordLength; i ++);
+};
+
+
+
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)};
+generateBtn.addEventListener("click", writePassword);
